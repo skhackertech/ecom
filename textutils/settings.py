64cf37f3083aaa3bbb2cdf0ce99cc7ebfe8a8964
skhackertech <40913947+skhackertech@users.noreply.github.com>
2021-01-07 11:17:24 +0530

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e_t)mplh&350&v&21hc52s*!3035lz$pwb^tg1a-7p+*_k=glo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','ksstoredemo.herokuapp.com']
 
 
 # Application definition
