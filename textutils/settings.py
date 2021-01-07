8484842db0d358161d92e11a13286961e1133d10
skhackertech <40913947+skhackertech@users.noreply.github.com>
2021-01-07 11:55:04 +0530

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
+import django_heroku
 
 import os
 
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
